3599e4875aa4f03ce5cba8529ca681d665d0a4f5
KaedeKagami86 <105937682+KaedeKagami86@users.noreply.github.com>
2025-09-09 14:27:13 -0400

    change to public for multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -8,7 +8,7 @@ public class Multiples {
         // why are we making another class instead of adding method to FizzBuzz class?
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int multipleCount = 0;
         for (int i = 1; i < n; i++) {
             boolean divisibleBy3 = i % a == 0;
@@ -26,7 +26,7 @@ public class Multiples {
     }
 
     // overloading
-    static int multiples() {
+    public static int multiples() {
         int multipleCount = 0;
         for (int i = 1; i < 1000; i++) {
             boolean divisibleBy3 = i % 3 == 0;
